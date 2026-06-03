dc91a158dc0fa40e2c21cfff711c089edc743e2c
szymon <szymon_jozef@proton.me>
2026-06-03 13:20:39 +0200

    refactor: move rstudio to university.nix

--- modules/features/home/cli/university/university.nix
+++ modules/features/home/cli/university/university.nix
@@ -1,11 +1,26 @@
 { ... }:
 {
   flake.homeModules.university =
-    { inputs, pkgs, ... }:
+    {
+      inputs,
+      pkgs,
+      pkgs-stable,
+      ...
+    }:
+    let
+      rstudio-with-packages = pkgs-stable.rstudioWrapper.override {
+        # we use stable, because unstable is broken rn
+        packages = with pkgs-stable.rPackages; [
+          # additional packages for rstudio here
+          ggplot2
+        ];
+      };
+    in
     {
       home.packages = [
         inputs.zut-calendar.packages.${pkgs.stdenv.hostPlatform.system}.default
         inputs.zutui.packages.${pkgs.stdenv.hostPlatform.system}.default
+        rstudio-with-packages
       ];
     };
 }
--- modules/features/home/gui/packages.nix
+++ modules/features/home/gui/packages.nix
@@ -3,24 +3,13 @@
   flake.homeModules.gui =
     {
       pkgs,
-      pkgs-stable,
       ...
     }:
-    let
-      rstudio-with-packages = pkgs-stable.rstudioWrapper.override {
-        # we use stable, because unstable is broken rn
-        packages = with pkgs-stable.rPackages; [
-          # additional packages for rstudio here
-          ggplot2
-        ];
-      };
-    in
     {
-      home.packages = with pkgs; [ # TODO! split into files
+      home.packages = with pkgs; [
+        # TODO! split into files
         homebank
         gimp
-        rstudio-with-packages
-
         feh
 
         # music
