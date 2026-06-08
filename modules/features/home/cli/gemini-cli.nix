004dcaafaa8087912cd3a63898070de6502823f9
szymon <szymon_jozef@proton.me>
2026-06-08 19:21:28 +0200

    chore: change gemini-cli package

--- modules/features/home/cli/gemini-cli.nix
+++ modules/features/home/cli/gemini-cli.nix
@@ -1,16 +1,18 @@
 { ... }:
 {
   flake.modules.homeManager.gemini =
-    { ... }:
+    { pkgs, ... }:
     {
       # Move this to a separate module?
       programs.antigravity-cli = {
         enable = true;
+        package = pkgs.gemini-cli;
+
         defaultModel = "gemini-3.1-pro-preview";
         context = {
           GEMINI = ''
             # Context
-            Pisz po polsku i sie nie wydurniaj.
+            Pisz po polsku i sie nie wydurniaj. Odpowiadaj krótko i zwęźle, nie dopytuj niepotrzebnie.
           '';
         };
       };
