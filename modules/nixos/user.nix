37f10a147c25a166133fcbae4ad0ecd5ad8e2000
szymon <szymon_jozef@proton.me>
2026-07-01 15:40:47 +0200

    feat: add ghostty to configuration

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -41,6 +41,8 @@
             self.modules.homeManager.ashell
             self.modules.homeManager.gui
             self.modules.homeManager.kitty
+            self.modules.homeManager.ghostty
+
             self.modules.homeManager.cli
             self.modules.homeManager.gemini
             self.modules.homeManager.browsers
