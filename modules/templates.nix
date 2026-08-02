82a065c573a7583116f1dbdf171ba46c7f6cfa90
szymon <szymon_jozef@proton.me>
2026-08-02 18:58:43 +0200

    fix: add new templates to templates.nix

--- modules/templates.nix
+++ modules/templates.nix
@@ -1,8 +1,18 @@
 {
   flake.templates = {
+    empty = {
+      path = ./_templates/empty;
+      description = "Empty flake template using flake-parts and import tree";
+    };
+
     cpp = {
       path = ./_templates/cpp;
       description = "C++ nix flake";
+    };
+
+    py = {
+      path = ./_templates/py;
+      description = "Python + uv nix flake";
     };
   };
 }
