aa5e6e33260def95cf8162377fc56e26916b62c7
szymon <szymon_jozef@proton.me>
2026-09-02 12:45:29 +0200

    chore: update rust template

--- modules/_templates/rs/nix/shell.nix
+++ modules/_templates/rs/nix/shell.nix
@@ -1,7 +1,10 @@
 {
   perSystem = { pkgs, config, ... }: {
     devShells.default = pkgs.mkShell {
-      packages = with pkgs; [ ];
+      packages = with pkgs; [
+        clippy
+        rustfmt
+      ];
 
       inputsFrom = [
         (config.packages.default)
