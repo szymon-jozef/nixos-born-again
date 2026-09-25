f7db3cfd0f3351858801ec0294d48b10f9d5d24b
szymon <szymon_jozef@proton.me>
2026-09-25 17:55:44 +0200

    refactor: add home-manager imports to pitagoras config

--- modules/hosts/nixos/pitagoras.nix
+++ modules/hosts/nixos/pitagoras.nix
@@ -15,52 +15,76 @@ in
     system = "x86_64-linux";
 
     modules = [
-      {
-        system.hostname = hostname;
-        monitors.primary = "eDP-1";
-      }
+      (
+        { config, ... }:
+        {
+          system.hostname = hostname;
+          monitors.primary = "eDP-1";
 
-      # general
-      self.commonModules.options
-      self.nixosModules.general
-      self.nixosModules.locale-polish
+          imports = [
+            # general
+            self.commonModules.options
+            self.nixosModules.general
+            self.nixosModules.locale-polish
 
-      # bootloader
-      self.nixosModules.bootloader-gpt
+            # bootloader
+            self.nixosModules.bootloader-gpt
 
-      # packages
-      self.nixosModules.packages
-      self.nixosModules.university
-      self.nixosModules.packages-virtualisation
-      self.nixosModules.gaming
+            # packages
+            self.nixosModules.packages
+            self.nixosModules.university
+            self.nixosModules.packages-virtualisation
+            self.nixosModules.gaming
 
-      self.nixosModules.programs
-      self.nixosModules.hyprland
-      self.nixosModules.cli
+            self.nixosModules.programs
+            self.nixosModules.hyprland
+            self.nixosModules.cli
 
-      # user
-      self.nixosModules.user
+            # user
+            self.nixosModules.user
 
-      # theme
-      self.nixosModules.theme
+            # theme
+            self.nixosModules.theme
 
-      # nvidia
-      self.nixosModules.nvidia
+            # nvidia
+            self.nixosModules.nvidia
 
-      # services
-      self.nixosModules.services
-      self.nixosModules.services-syncthing
-      self.nixosModules.services-snapper
+            # services
+            self.nixosModules.services
+            self.nixosModules.services-syncthing
+            self.nixosModules.services-snapper
 
-      # display manager
-      self.nixosModules.display-manager-autologin
+            # display manager
+            self.nixosModules.display-manager-autologin
 
-      # network
-      self.nixosModules.network
-      self.nixosModules.network-home-wifi
+            # network
+            self.nixosModules.network
+            self.nixosModules.network-home-wifi
 
-      # hardware
-      self.nixosModules.hardware-pitagoras
+            # hardware
+            self.nixosModules.hardware-pitagoras
+          ];
+
+          # home-manager imports go here
+          home-manager.users.${config.user.name} = {
+            imports = [
+              # self.modules.homeManager.* import
+              self.modules.homeManager.theme
+              self.modules.homeManager.hypr
+              self.modules.homeManager.ashell
+              self.modules.homeManager.gui
+              self.modules.homeManager.ghostty
+
+              self.modules.homeManager.cli
+              self.modules.homeManager.gemini
+              self.modules.homeManager.browsers
+              self.modules.homeManager.xdg
+              self.modules.homeManager.gaming
+              self.modules.homeManager.university
+            ];
+          };
+        }
+      )
     ];
   };
 }
