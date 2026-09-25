44c3c12a3033f101c1a41480fc3e48aa82141269
szymon <szymon_jozef@proton.me>
2026-09-25 17:50:27 +0200

    refactor: remove home-manager imports from user.nix

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -44,22 +44,8 @@
           imports = [
             inputs.hyprcursor-phinger.homeManagerModules.hyprcursor-phinger
             inputs.catppuccin.homeModules.catppuccin
-
-            # self.modules.homeManager.* import
-            self.modules.homeManager.theme
-            self.modules.homeManager.hypr
-            self.modules.homeManager.ashell
-            self.modules.homeManager.gui
-            self.modules.homeManager.kitty
-            self.modules.homeManager.ghostty
-
-            self.modules.homeManager.cli
-            self.modules.homeManager.gemini
-            self.modules.homeManager.browsers
-            self.modules.homeManager.xdg
-            self.modules.homeManager.gaming
-            self.modules.homeManager.university
           ];
+
           home = {
             file = {
               "Obrazy/tapety/catppuccin".source = inputs.wallpapers;
